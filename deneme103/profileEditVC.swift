44289c96ec50a26b631c188fbdbc9f46a0cf97af
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2023-03-21 23:34:06 +0300

    deneme

--- deneme103/profileEditVC.swift
+++ deneme103/profileEditVC.swift
@@ -14,6 +14,7 @@ import FirebaseFirestore
 
 class profileEditVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //check
     let profileImage = UIImageView()
     let usernameText = UITextField()
     
